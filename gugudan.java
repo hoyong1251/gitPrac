745c3a789b0e982ca52422f7679e8233c406db70
hoyong <hy9631883@gmail.com>
2020-04-22 04:29:04 +0900

    v1.5 : input-zero

--- gugudan.java
+++ gugudan.java
@@ -9,8 +9,18 @@ public class gugudan {
 		Scanner scan=new Scanner(System.in);
 		System.out.print("몇 단 출력?: ");
 		int dan=scan.nextInt();
+		if(dan==0) {
+			for(int i=2; i<10; i++) {
+				for(int j=1; j<10; j++) {
+					System.out.print(i+" X "+j+" = "+i*j+" ");
+				}
+				System.out.print("\n");
+			}
+		}
+		else {
 		for(int i=1; i<10; i++) {
 			System.out.println(dan+" X "+i+" = "+dan*i);
+			}
 		}
 		scan.close();
 	}
